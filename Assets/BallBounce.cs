222803819763ad87dd4519559da5a9b554f2632b
clickndrag <virtineaurum@gmail.com>
2020-09-15 21:03:14 +0900

    Ball Bounce Overhaul
    Added Bounce Acceleration
    Added Bounce Max Height Reduction

--- Assets/BallBounce.cs
+++ Assets/BallBounce.cs
@@ -6,13 +6,21 @@ public class BallBounce : MonoBehaviour
 {
     // Set Variables
     Vector3 pos;
-    public float bounceSpeed = 1.0f;
-    public float bounceAmount = 2.0f;
+    static public float bounceSpeed = 1.0f;
+    static public float bounceHeight = 1.0f;
+
+    // Set Acceleration and Gravity
+    public float Accelerate = 1.0001f;
+    public float Gravity = 0.999f;
+
+    private float ballSpeed;
+    private float ballMaxHeight;
+    private float ballHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-    
+
     }
 
     // Update is called once per frame
@@ -21,8 +29,23 @@ public class BallBounce : MonoBehaviour
         //pos에 오브젝트의 위치 대입
         pos = gameObject.transform.position;
 
-        //Bounce
-        gameObject.transform.position = new Vector3(pos.x, Mathf.Sin(Mathf.Clamp01((Time.time * bounceSpeed) % 1) * Mathf.PI) * bounceAmount + 0.5f, pos.z);
-    }
+        //Bounce Reduction
+        ballHeight = Mathf.Sin(Mathf.Clamp01((Time.time * ballSpeed) % 1.0f) * Mathf.PI);
+        gameObject.transform.position = new Vector3(pos.x, ballHeight * ballMaxHeight + 0.5f, pos.z);
 
+        //Accelerate Drop Speed and Reduce Max Height
+        ballSpeed *= Accelerate;
+        ballMaxHeight *= Gravity;
+
+        //Bounce Stop Control
+        if (ballMaxHeight <= 0.01f)
+            ballMaxHeight = 0.0f;
+
+        //Clear Bounce Status
+        if (Input.GetMouseButtonDown(0))
+        {
+            ballSpeed = bounceSpeed;
+            ballMaxHeight = bounceHeight;
+        }
+    }
 }
